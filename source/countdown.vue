0ca4d9424ee8924f76bbd9ac1b44a85958ac94f2
Sam Pedley <sjpeds@gmail.com>
2017-11-21 05:30:41 -0600

    eh... i guess it's ok for now...

--- source/countdown.vue
+++ source/countdown.vue
@@ -10,12 +10,32 @@
 <script>
   export default {
     data() {
-      return {
-        days: '50',
-        hours: '20',
-        minutes: '10',
-        seconds: '4'
-      };
+        return {
+            now: Math.trunc((new Date()).getTime() / 1000),
+            date: Math.trunc(Date.parse("8/4/2018") / 1000)
+        }
+    },
+    computed: {
+    seconds() {
+        return (this.date - this.now) % 60;
+    },
+
+    minutes() {
+        return Math.trunc((this.date - this.now) / 60) % 60;
+    },
+
+    hours() {
+        return Math.trunc((this.date - this.now) / 60 / 60) % 24;
+    },
+
+    days() {
+        return Math.trunc((this.date - this.now) / 60 / 60 / 24);
+    }
+},
+    created() {
+        window.setInterval(() => {
+        this.now = Math.trunc((new Date()).getTime() / 1000);
+    },1000);
     }
   };
 </script>
