c6a48693bd8041da2f90e8d1027763e4c41a52e7
Sam Pedley <sjpeds@gmail.com>
2018-01-09 16:56:39 -0600

    added kohls link

---
+++ source/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>hi</h1>
+    <countdown></countdown>
+  </div>
+</template>
+
+<script>
+import Countdown from './components/countdown'
+export default {
+  components: {
+    Countdown
+  },
+  name: 'app'
+}
+</script>
---
+++ source/components/countdown.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    {{days}} Days {{hours}} Hours {{minutes}} Minutes {{seconds}} Seconds
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        now: Math.trunc((new Date()).getTime() / 1000),
+        date: Math.trunc(Date.parse("8/4/2018") / 1000)
+        }
+    },
+    computed: {
+      seconds() {
+        return (this.date - this.now) % 60
+      },
+
+      minutes() {
+        return Math.trunc((this.date - this.now) / 60) % 60
+      },
+
+      hours() {
+        return Math.trunc((this.date - this.now) / 60 / 60) % 24
+      },
+
+      days() {
+        return Math.trunc((this.date - this.now) / 60 / 60 / 24)
+      }
+    },
+    created() {
+      window.setInterval(() => {
+        this.now = Math.trunc((new Date()).getTime() / 1000)
+    },1000)
+    }
+  };
+</script>
